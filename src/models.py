6c4645de2c16a5ab5cb0b059022a3f693f1a71dc
Techkene <keneanoliefo24@gmail.com>
2024-04-02 18:05:51 +0100

    Introduce Question class for managing survey questions

--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ from werkzeug.security import (
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.column(db.Interger, primary_key=True)
     username = db.column(db.Strings(80), unique=True, nullable=False)
     email = db.column(db.Strings(80), unique=True, nullable=False)
@@ -29,3 +29,18 @@ class User(db.Model):
     def __repr__(self):
         return f"User({self.id}, '{self.name}')"
 
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, unique=False, nullable=False)
+    option1 = db.Column(db.String, nullable=False)
+    option2 = db.Column(db.String, nullable=False)
+    option3 = db.Column(db.String, nullable=False)
+    option4 = db.Column(db.String, nullable=False)
+    correct_option = db.Column(db.CHAR, nullable=False)
+    verified = db.Column(db.Boolean, nullable=False, default=False)
+    creator_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    question_set_id = db.Column(db.Integer, db.ForeignKey("questionset.id"), nullable=True)
+
+    def __repr__(self):
+        return f"Question({self.id}, '{self.question}', {self.creator})"
