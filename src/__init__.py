3660f4df18a9caa98a6e2a85f4c8940a5d794318
Techkene <keneanoliefo24@gmail.com>
2024-04-03 14:32:24 +0100

    This commit integrates flask-jwt-extended library into the project for handling token-based authentication. This package provides extended functionality for JWT token creation, decoding, and verification, enhancing the security and authentication features of the application.

--- src/__init__.py
+++ src/__init__.py
@@ -2,19 +2,22 @@ from flask import Flask
 import os
 from src.authentication import auth
 from src.models import db
+from flask_jwt_extended import JWTManager
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY = os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI")
+            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI"),
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
         )
     else:
         app.config.from_mapping(test_config)
 
     db.app = app
     db.init_app(app)
+    JWTManager(app)
     app.register_blueprint(auth)
     
     return app
