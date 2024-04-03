09efb055c9e177b83fc174f96a2481907c207da9
Techkene <keneanoliefo24@gmail.com>
2024-04-03 10:47:21 +0100

    Implemented SQLAlchemy integration for database connectivity and updated models.py to include a new table for storing user data and questions. This commit establishes the foundation for robust data management within the application.

--- src/models.py
+++ src/models.py
@@ -10,12 +10,12 @@ from werkzeug.security import (
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
-    id = db.column(db.Interger, primary_key=True)
-    username = db.column(db.Strings(80), unique=True, nullable=False)
-    email = db.column(db.Strings(80), unique=True, nullable=False)
-    password = db.column(db.Text(120), nullable=False)
-    created_at = db.column(db.DateTime, default=datetime.now())
-    updated_at = db.column(db.DateTime, onupdate=datetime.now())
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.Text(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
     questions = db.relationship("Question", backref="creator")
     is_admin = db.Column(db.Boolean, default=False)
     result = db.relationship("Result", backref="user")
@@ -41,6 +41,29 @@ class Question(db.Model):
     verified = db.Column(db.Boolean, nullable=False, default=False)
     creator_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     question_set_id = db.Column(db.Integer, db.ForeignKey("questionset.id"), nullable=True)
-
+    
     def __repr__(self):
         return f"Question({self.id}, '{self.question}', {self.creator})"
+
+
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    total_number = db.Column(db.Integer, nullable=False)
+    correct = db.Column(db.Integer, default=0, nullable=False)
+    not_attempt = db.Column(db.Integer, default=0, nullable=False)
+    incorrect = db.Column(db.Integer, default=0, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Result({self.id}, {self.correct}/{self.total_number})"
+
+
+class QuestionSet(db.Model):
+    __tablename__ = 'questionset'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=True)
+    questions = db.relationship("Question", backref="qset")
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"QuestionSet({self.id}, {[i.id for i in self.questions]})"
