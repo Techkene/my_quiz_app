92a4a0c15a435bf3fb64357776420f2f24cff0da
Techkene <keneanoliefo24@gmail.com>
2024-04-03 15:10:13 +0100

    Adds the implementation of a user login endpoint in the authentication Blueprint(authentication.py file). Here flask-jwt-extended creates a token for the new user.

--- src/authentication.py
+++ src/authentication.py
@@ -3,6 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import validators
 from src.models import User, db
 from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
+from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token, get_jwt_identity
 
 auth = Blueprint("auth", __name__)
 
@@ -41,6 +42,33 @@ def register():
 
     }), HTTP_201_CREATED
     return "User created"
+
+@auth.post('/login')
+def login():
+    email = request.json.get('email', '')
+    password = request.json.get('password', '')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        is_pass_correct = check_password_hash(user.password, password)
+
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=user.id)
+            access = create_access_token(identity=user.id)
+
+            return jsonify({
+                'user': {
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }
+
+            }), HTTP_200_OK
+
+    return jsonify({'error': 'Wrong credentials'}), HTTP_401_UNAUTHORIZED
+
 
 @auth.get("/me")
 def me():
